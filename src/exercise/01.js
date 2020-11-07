1d1c63e0e9f1af472aa6b67c714d52a8911b1824
Jerick Bilalat <jerickbilalat@jrk-air.local>
2020-11-06 18:36:29 -0600

    finish first exercise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,10 +5,12 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState(null)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    const value = event.target.value
+    setName(value)
   }
 
   return (
